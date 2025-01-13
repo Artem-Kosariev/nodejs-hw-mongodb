6ac92ceaaf40c7273ebc3547df8ade2a5bff4639
Artem Kosariev <rkosyxa@gmail.com>
2025-01-13 10:01:02 +0500

    final task 2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -7,13 +7,15 @@ export const initMongoConnection = async () => {
   if (
     !process.env.MONGODB_USER ||
     !process.env.MONGODB_PASSWORD ||
-    !process.env.MONGODB_DB
+    !process.env.MONGODB_DB ||
+    !process.env.MONGODB_URL ||
+    !process.env.PORT
   ) {
     console.error('Missing MongoDB credentials in the environment variables');
     process.exit(1);
   }
 
-  const mongoURL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.9rgdj.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+  const mongoURL = `${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
   try {
     await mongoose.connect(mongoURL);
